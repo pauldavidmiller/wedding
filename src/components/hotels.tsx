c463f9ec2c21a69dc69c354584e5239717b6de32
americanaandcompany <admin@americanaandcompany.com>
2025-02-05 19:53:32 -0500

    Updated Hotels Data and Added Block Reservations

--- src/components/hotels.tsx
+++ src/components/hotels.tsx
@@ -13,19 +13,32 @@ const HotelsSection = () => {
       <div className="hotels-list">
         {hotelsData.map((hotel, index) => (
           <div className="hotel-item" key={index}>
-            <h3 className="hotel-name">{hotel.name}</h3>
-            <p className="hotel-address">{hotel.address}</p>
-            <p className="hotel-phone">
-              <a href="tel:+2014463301">{hotel.phone}</a>
-            </p>
-            <a
-              className="hotel-link"
-              href={hotel.link}
-              target="_blank"
-              rel="noopener noreferrer"
+            <div className="flex flex-col">
+              <h3 className="hotel-name">{hotel.name}</h3>
+              <p className="hotel-address">{hotel.address}</p>
+              <div className="flex flex-wrap gap-4 justify-center">
+                <p className="hotel-phone">
+                  <a href={`tel:+${hotel.phoneNumber}`}>
+                    {hotel.phoneNumberString}
+                  </a>
+                </p>
+                <a
+                  className="hotel-link"
+                  href={hotel.website}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Website Link
+                </a>
+              </div>
+            </div>
+            <button
+              type="button"
+              className="hotel-block-button"
+              onClick={() => window.open(hotel.reservationLink, "_blank")}
             >
-              Website Link
-            </a>
+              Reserve With Our Wedding Block
+            </button>
             <img alt="Hotel Directions" src={hotel.image} />
           </div>
         ))}
