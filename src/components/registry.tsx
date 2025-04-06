43253dbb8ecdf7f69d3e566a40528aecca6bb363
americanaandcompany <admin@americanaandcompany.com>
2025-04-06 17:14:23 -0400

    Added registries

--- src/components/registry.tsx
+++ src/components/registry.tsx
@@ -4,9 +4,33 @@ import { Section } from "../types/section";
 import { useAppContext } from "../contexts/app-context";
 import { isDevelopment } from "../extensions/environments";
 
+type Registry = {
+  name: string;
+  img: string;
+  url: string;
+};
+
 const RegistrySection = () => {
   const { registryReleaseDate } = useAppContext();
   const hideRegistries = new Date() < registryReleaseDate && !isDevelopment();
+
+  const registries: Registry[] = [
+    {
+      name: "Crate & Barrel",
+      img: "/images/crateandbarrellogo.png",
+      url: "https://www.crateandbarrel.com/gift-registry/margot-bailowitz-and-paul-miller/r7298090",
+    },
+    {
+      name: "Williams Sonoma",
+      img: "/images/williamssonomalogo.png",
+      url: "https://www.williams-sonoma.com/registry/qwf5hhrb7r/registry-list.html",
+    },
+    {
+      name: "Macy's",
+      img: "/images/macyslogo.png",
+      url: "https://www.macys.com/registry/Margot-Bailowitz-Paul-Miller/1055428",
+    },
+  ];
 
   return (
     <PageSection
@@ -24,17 +48,14 @@ const RegistrySection = () => {
               you so much for all of your generosity!
             </p>
             <div className="registry-images">
-              <img
-                src="/images/crateandbarrellogo.png"
-                alt="Crate & Barrel"
-                className="w-12 h-auto"
-                onClick={() =>
-                  window.open(
-                    "https://www.crateandbarrel.com/gift-registry/margot-bailowitz/r7269468",
-                    "_blank"
-                  )
-                }
-              />
+              {registries.map((r) => (
+                <img
+                  src={r.img}
+                  alt={r.name}
+                  className="w-12 h-auto"
+                  onClick={() => window.open(r.url, "_blank")}
+                />
+              ))}
             </div>
           </>
         )}
